4f9ddbe000a6a4d19be3e49bf2d65b7540d21751
Monarch Wadia <monarchwadia@gmail.com>
2023-11-25 18:53:14 -0500

    Lojong Timer

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -19,9 +19,10 @@
 		return { chapterTitle, slogan };
 	};
 
-	let isRunning = $state(true);
+	let isRunning = $state(false);
 
 	let sloganInterval = $state(0);
+	let pageDuration = $state(60);
 	let slogan = $state(getRandomSlogan());
 
 	function startSession() {
@@ -30,7 +31,7 @@
 		// @ts-expect-error - typescript doesn't know about setInterval
 		sloganInterval = setInterval(() => {
 			slogan = getRandomSlogan();
-		}, 5 * 1000);
+		}, pageDuration * 1000);
 		slogan = getRandomSlogan();
 	}
 
@@ -44,11 +45,26 @@
 	<div class="flex flex-col max-w-5xl gap-4">
 		{#if isRunning}
 			<div class="text-2xl sm:text-3xl md:text-4xl lg:text-4xl text-primary">{slogan.slogan}</div>
-			<div class="italic opacity-40">{slogan.chapterTitle}</div>
+			<div class="italic opacity-50">{slogan.chapterTitle}</div>
 			<button class="btn btn-ghost w-fit opacity-40" on:click={() => stopSession()}>Stop</button>
 		{:else}
-			<div class="text-4xl">Lojong Timer</div>
-			<button class="btn btn-primary w-fit" on:click={() => startSession()}> Start </button>
+			<div class="text-4xl text-primary">Lojong Timer</div>
+			<p>A simple timer that displays Lojong slogans.</p>
+			<div class="flex flex-col gap-4">
+				<button class="btn btn-primary w-fit" on:click={() => startSession()}> Start </button>
+				<!-- a numeric input to select seconds of practice -->
+				<div>
+					<label for="step-duration" class="label"> Duration (Seconds) </label>
+					<input
+						class="input input-md"
+						id="step-duration"
+						name="step-duration"
+						type="number"
+						min="1"
+						bind:value={pageDuration}
+					/>
+				</div>
+			</div>
 		{/if}
 	</div>
 </div>
