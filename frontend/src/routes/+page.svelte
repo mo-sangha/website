3b9a887467b830ec9febdb3e0d8ce35d5650f706
Monarch Wadia <monarchwadia@gmail.com>
2023-11-25 18:23:18 -0500

    Added lojong

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,53 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+	import * as lojong from '$lib/lojong';
+
+	/**
+	 * Pick a random item from an array
+	 * @template T
+	 * @param {T[]} array - the array to pick from
+	 * @returns {T} a random item from the array
+	 */
+	const pickRandom = (array) => {
+		const randomIndex = Math.floor(Math.random() * array.length);
+		return array[randomIndex];
+	};
+
+	const getRandomSlogan = () => {
+		const chapter = pickRandom(lojong.slogans);
+		const chapterTitle = chapter.title;
+		const slogan = pickRandom(chapter.slogans);
+		return { chapterTitle, slogan };
+	};
+
+	let isRunning = $state(false);
+
+	let sloganInterval = $state(0);
+	let slogan = $state(getRandomSlogan());
+
+	function startSession() {
+		isRunning = true;
+
+		// @ts-expect-error - typescript doesn't know about setInterval
+		sloganInterval = setInterval(() => {
+			slogan = getRandomSlogan();
+		}, 5 * 1000);
+		slogan = getRandomSlogan();
+	}
+
+	function stopSession() {
+		clearInterval(sloganInterval);
+		isRunning = false;
+	}
+</script>
+
+{#if isRunning}
+	<!-- a button that says start -->
+	<div>{slogan.slogan}</div>
+	<div>{slogan.chapterTitle}</div>
+	<button on:click={() => stopSession()}>Stop</button>
+{:else}
+	<button on:click={() => startSession()}>Start</button>
+{/if}
 
 <style lang="postcss">
 	:global(html) {
