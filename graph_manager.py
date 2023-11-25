e31410f32aa0129fa12c840934ad9d9494b32847
Monarch Wadia <monarchwadia@gmail.com>
2023-11-25 17:39:06 -0500

    Nonworking backend

--- graph_manager.py
+++ graph_manager.py
@@ -1,9 +1,12 @@
+from typing import Generic, TypeVar
 from rdflib import Graph, URIRef, Literal
 
-class GraphManager():
+AdditionalData = TypeVar("AdditionalData")
+
+class GraphManager(Generic[AdditionalData]):
     g = Graph()
 
-    def add(self, subject: str, predicate: str, object: str):
+    def add(self, subject: str, predicate: str, object: str, additional_data: AdditionalData = None):
         self.g.add((Literal(subject), Literal(predicate), Literal(object)))
     
     def query(self, query: str):
