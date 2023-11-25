ae00e653e5b5ae8435d80f0d05c7cf71e6187e14
Monarch Wadia <monarchwadia@gmail.com>
2023-11-24 20:33:57 -0500

    Cleaned it up

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from rdflib import Graph, URIRef, Literal
 from graph_manager import GraphManager
 
-# Create a new graph
 gm = GraphManager()
 
-# Add triples to the graph
 gm.add("foo", "edge", "bar")
 gm.add("bar", "edge", "baz")
 gm.add("baz", "edge", "foo")
@@ -19,9 +17,4 @@ WHERE {
 
 print(response.serialize().decode("utf-8"))
 
-# Serialize the graph to a string in Turtle format
-# response.serialize(destination="graph.ttl")
-
 gm.save()
-
-# g.serialize(destination="graph.ttl", format="turtle")
