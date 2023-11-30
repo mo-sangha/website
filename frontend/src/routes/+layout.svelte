c33384dc4433eb02cde4ebf92bec165e63940284
Monarch Wadia <monarchwadia@gmail.com>
2023-11-30 16:15:05 -0500

    Meditation online begun

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -6,4 +6,22 @@
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
-<slot />
+<div class="h-full w-full flex flex-col px-4">
+	<!-- Navbar -->
+	<div class="navbar bg-base-100">
+		<div class="flex-1">
+			<a class="text-xl font-bold" href="/">Meditation Online</a>
+		</div>
+		<div class="flex-none">
+			<ul class="menu menu-horizontal px-1">
+				<li><a href="/tools">Tools</a></li>
+				<li><a href="/about">About</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<!-- Body -->
+	<div class="flex-grow">
+		<slot />
+	</div>
+</div>
---
+++ frontend/src/routes/tools/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	/** @type {{ title: string, description: string, link: string }[]} */
+	const tools = [
+		{
+			title: 'Lojong Timer',
+			description:
+				'A simple, configurable timer that rotates through Lojong phrases every few seconds. No bells or animations.',
+			link: '/tools/lojong-timer'
+		}
+	];
+</script>
+
+<div class="flex flex-col gap-4">
+	{#each tools as tool}
+		<div class="card outline max-w-xs">
+			<div class="card-body">
+				<div class="card-title">{tool.title}</div>
+				<p>{tool.description}</p>
+				<a class="btn btn-sm" href={tool.link}>Go</a>
+			</div>
+		</div>
+	{/each}
+</div>
