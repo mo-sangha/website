a8bd9ed917cb9b45d599895ba0da8403503cbee8
Monarch Wadia <monarchwadia@gmail.com>
2023-11-30 19:15:28 -0500

    Front page styling

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,11 +1,11 @@
-<div class="hero min-h-[50vh] bg-base-200">
+<div class="hero min-h-[50vh] bg-primary bg-opacity-10">
 	<div class="hero-content text-center">
 		<div class="max-w-md">
-			<h1 class="text-5xl font-bold">Daily Meditation.</h1>
+			<h1 class="text-4xl font-bold">Live Meditation Sessions</h1>
 			<div class="py-6 text-left mx-auto w-fit">
 				<ul class="list-disc">
+					<li>Free to join online. All are welcome.</li>
 					<li>Twice a day: 9am and 9pm ET.</li>
-					<li>Free to join. All are welcome.</li>
 					<li>Led by Lama Dawai Gocha.</li>
 					<li>Established 2013.</li>
 				</ul>
