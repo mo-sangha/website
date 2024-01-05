98e00c495adcb5c0e4c48b8a701b91e46a04e07e
Monarch Wadia <monarchwadia@gmail.com>
2024-01-05 17:47:10 -0500

    Added format.sh instructions

--- frontend/asset-projects/originals/format.sh
+++ frontend/asset-projects/originals/format.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script will resize all images in the current directory to 800x800px and save them in the frontend/static folder as "photo-<filename>.jpg"
+
 for file in *.png *.jpg; do 
   convert "$file" -resize 800x800^ -gravity center -extent 800x800 -quality 80 "../../static/photo-${file%.*}.jpg"; 
 done
