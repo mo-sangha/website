d68db6c3f3a92bc30e314686c630c98eb3b1a0c2
Monarch Wadia <monarchwadia@gmail.com>
2024-01-05 19:49:06 -0500

    Our Lineage

--- frontend/src/components/Photo.svelte
+++ frontend/src/components/Photo.svelte
@@ -7,7 +7,7 @@
 	<img
 		{src}
 		alt={caption || src}
-		class="max-w-[250px] border-primary shadow-sm shadow-slate-500 border-2 rounded-xl"
+		class="max-w-[250px] border-primary shadow-sm shadow-slate-500 border-2"
 	/>
 	{#if caption}
 		<figcaption class="text-center text-sm text-gray-500">{caption}</figcaption>
--- frontend/src/routes/about/our-lineage/+page.svelte
+++ frontend/src/routes/about/our-lineage/+page.svelte
@@ -1,23 +1,59 @@
+<script>
+	import Photo from '../../../components/Photo.svelte';
+</script>
+
 <div class="h-full w-full flex flex-col gap-8 md:gap-16">
 	<h2 class="page-header">Our Lineage</h2>
 	<div class="page-content-section">
 		<p>
-			This group is intended to be all inclusive and modern in the sense of creating a new kind of
-			space. Every person can have a voice and a kind of ownership within the group. Traditionally
-			it's known that every sentient being is ultimately a Buddha so in that sense we can empower
-			one another with minimum use of hierarchy while still preserving lineage and transmission. A
-			grass roots, very human, and accessible approach presented in harmony with modern science and
-			traditional methodology.
+			We are descended from the authentic Tibetan Buddhist Nyingma Palyul lineage. Our group's main
+			teacher, <a class="link-hover text-secondary" href="/about/dawai-gocha">dawai</a>, has
+			practiced for more than 20 years under the guidance of many great masters from that lineage,
+			and teaches what he has learned from them about Dzogchen, also known as Atiyoga.
 		</p>
-
+		<h3 class="font-display text-2xl">His Holiness Penor Rinpoche</h3>
+		<Photo src="/photo-penor-rinpoche.jpg" caption="HH Penor Rinpoche" />
 		<p>
-			The lineage and transmission is very important with practices like Dzogchen, Mahamudra and
-			Zen. Getting empowerments from living recognized masters, following your teachers advice, and
-			doing any preliminary practices is recommended. If the practitioner is ready then they can
-			begin awareness practices which, in a modern sense, can start with an introduction to
-			awareness.
+			His Holiness Penor Rinpoche (Tib. པད་ནོར་རིན་པོ་ཆེ་, Wyl. pad nor rin po che) or Kyabjé
+			Drubwang Pema Norbu Rinpoche (1932-2009) was one of the most well-known Nyingma and Dzogchen
+			masters of the twentieth century. Rinpoche was the third head of the Nyingma school of Tibetan
+			Buddhism and the eleventh throne-holder of the Palyul lineage. Considered an incarnation of
+			Vimalamitra, Rinpoche received his full spiritual training in Tibet before escaping to India
+			in 1959. He subsequently established Namdroling Monastery in Southern India and also taught
+			extensively throughout the world. Rinpoche had many Tibetan and international students, and in
+			the 1980s he recognized the first Western female reincarnate lama, Jetsunma Ahkon Lhamo (Alyce
+			Louise Zeoli).
 		</p>
-
-		<p>Please come and go as you feel, there are no rules about attendance.</p>
+		<h3 class="font-display text-2xl">Khencheng Tsewang Gyatso Rinpoche</h3>
+		<Photo src="/photo-khencheng-tsewang-gyatso.jpg" caption="Khencheng Tsewang Gyatso Rinpoche" />
+		<p>
+			Khenchen Tsewang Gyatso Rinpoche is a senior Khenpo (professor of Buddhist Philosophy) from
+			the Shedra (Lama's College) at Paylul Namdroling Monastery near Mysore in the south of India.
+			He is a representative of His Holiness Penor Rinpoche, throne-holder of Palyul Monastery and
+			its many branches around the world. H.H. Penor Rinpoche has entrusted him with the
+			establishment of Dharma Centers on behalf of His Holiness and the Palyul lineage in this
+			country. Khenchen Tsewang Gyatso has received all of the major lineage empowerments and
+			transmissions of the Nyingmapa school, including the Dudjom Tersar from Kyabje Dudjom
+			Rinpoche; the Longchen Nyingthig Yabshi, and Nyingma Kama from Dilgo Khyentse Rinpoche; and
+			the Rinchen Terdzod and Nam Chos from Pema Norbu Rinpoche. He has done intensive dzogchen
+			practice under the guidance of Penor Rinpoche and other eminent masters including Khenpo Jigme
+			Phuntsok Rinpoche and Nyoshul Khen Rinpoche. Pema Norbu Rinpoche has authorized him to
+			represent the monastery and to teach, to confer empowerments and to give personal instruction
+			in Buddhist practice.
+		</p>
+		<h3 class="font-display text-2xl">Lama Karma Dorje</h3>
+		<Photo src="/photo-karma-dorje.jpg" caption="Lama Karma Dorje" />
+		<p>
+			Venerable Lama Karma Dorje was born in a village near Gangtok, Sikkim. He began his Buddhist
+			training at the age of twelve and became a novice monk at the age of 30, at Sonada Monastery
+			in Darjeeling with H. E. Kalu Rinpoche. He later took full ordination at Rumtek, Sikkim, the
+			monastery of His Holiness the Karmapa, and then completed the three-year retreat in Sonada. He
+			is an accomplished painter in the thangka tradition, the sacred art of Tibet. In 1980 His
+			Eminence Kalu Rinpoche and H.H. Karmapa requested that Lama Dorje visit various centers in
+			Europe, France, New York, Los Angeles, and Seattle. In 1981 His Eminence Kalu Rinpoche asked
+			him to come to Santa Fe, where he has remained as director of KSK Buddhist Center. Venerable
+			Lama Dorje is also the director of the Dharma Center in Taos, Kagyu Degyi Choling, where he
+			supervised the construction of a 30-foot Stupa, the Gonpa, and the Kangyur building.
+		</p>
 	</div>
 </div>
