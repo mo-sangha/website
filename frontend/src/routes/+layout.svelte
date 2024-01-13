5724e0949371777a7cb016b884a9a657cf24d0c5
Monarch Wadia <monarchwadia@gmail.com>
2024-01-13 13:52:26 -0500

    Added youtube to footer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -33,21 +33,27 @@
 	<footer
 		class="footer p-10 mt-10 bg-gradient-to-b from-base-100 to-base-300 text-neutral-content p-gutter"
 	>
-		<nav>
-			<header class="footer-title">Site Map</header>
-			<!-- <a href="/" class="link link-hover">Home</a>
-			<a href="/tools" class="link link-hover">Tools</a>
-			<a href="/about" class="link link-hover">About</a> -->
-
-			{#each links as link}
-				{#if 'children' in link}
-					{#each link.children as child}
-						<a href={child.href} class="link link-hover">{link.name} -> {child.name}</a>
-					{/each}
-				{:else}
-					<a href={link.href} class="link link-hover">{link.name}</a>
-				{/if}
-			{/each}
+		<nav class="flex flex-row gap-8">
+			<div class="flex flex-col">
+				<header class="footer-title">Site Map</header>
+				<!-- <a href="/" class="link link-hover">Home</a>
+				<a href="/tools" class="link link-hover">Tools</a>
+				<a href="/about" class="link link-hover">About</a> -->
+	
+				{#each links as link}
+					{#if 'children' in link}
+						{#each link.children as child}
+							<a href={child.href} class="link link-hover">{link.name} -> {child.name}</a>
+						{/each}
+					{:else}
+						<a href={link.href} class="link link-hover">{link.name}</a>
+					{/if}
+				{/each}
+			</div>
+			<div class="flex flex-col">
+				<header class="footer-title">Socials</header>
+				<a href="https://www.youtube.com/@JoinMeditationOnline" class="link link-hover">YouTube</a>
+			</div>
 		</nav>
 	</footer>
 </div>
