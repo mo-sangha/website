da5de126064e407392cdb0a40ca4296cff267e4a
Dorje <dorje@hidden.github.com>
2025-04-22 09:43:50 -0600

    feat: Remove the outdated Friends page
    The meditation sessions listed on the Friends page are not actually
    occurring, and therefore links to them are misleading.

--- src/routes/navLinks.ts
+++ src/routes/navLinks.ts
@@ -19,6 +19,5 @@ export const links: NavLink[] = [
 			{ name: 'Our Lineage', href: '/about/our-lineage', type: 'leaf' },
 			{ name: 'Our Group', href: '/about/our-group', type: 'leaf' }
 		]
-	},
-	{ name: 'Friends', href: '/friends', type: 'leaf' }
+	}
 ];
