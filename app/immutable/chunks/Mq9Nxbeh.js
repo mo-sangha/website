9be2e701c70f022605ac4ffe11683b8c24e033c1
Dorje <dorje@hidden.github.com>
2025-04-29 11:59:37 -0600

    deploy: fix: Add back the Paypal link alongside the Kofi link
    04e6733bfd7cd2cc22aed3171c28937146183220

---
+++ app/immutable/chunks/Mq9Nxbeh.js
@@ -0,0 +1,25 @@
+import{t as m,a as s,c as D}from"./EiX_PXJ2.js";import"./DBYP0D2S.js";import{ai as x,c as h,b as g,t as G,ak as _,aj as I,a4 as C,N as L,k as $,s as B}from"./DlmVDik5.js";import{i as M}from"./C8RyWJ1q.js";import{s as l,a as J,b as N}from"./DFO8vX4A.js";import{i as k}from"./BKpyWtRi.js";import{o as P}from"./dL0xOHMm.js";import{l as z,p as b}from"./wmW3eoV9.js";var K=m('<div><svg xmlns="http://www.w3.org/2000/svg"><path></path></svg></div>');function R(p,t){const r=z(t,["children","$$slots","$$events","$$legacy"]),o=z(r,["icon","color","dimension"]);x(t,!1);let c=b(t,"icon",8),i=b(t,"color",8),n=b(t,"dimension",8,"1rem");const[a,e,v,E,A]=c().icon;k();var f=K();let y;var d=h(f);l(d,"viewBox",`0 0 ${a} ${e}`);var w=h(d);l(w,"d",A),g(d),g(f),G(()=>{y=J(f,y,{class:"flex bg-red-50 rounded-full align-middle items-center p-1",...o}),l(d,"height",n()),l(d,"width",n()),l(w,"fill",i()),l(w,"stroke",i())}),s(p,f),_()}/*!
+ * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
+ * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+ * Copyright 2024 Fonticons, Inc.
+ */const j={prefix:"fas",iconName:"chevron-right",icon:[320,512,[9002],"f054","M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"]},u=""+new URL("../assets/icon-small.CnzeInJV.png",import.meta.url).href;var S=m(`<div class="w-[200px] h-[3.75rem]
+               bg-primary/30 rounded-md animate-pulse
+               mx-auto inline-flex"></div>`),U=m(`<a href="https://www.paypal.com/paypalme/dawaigocha" target="_blank" class="relative flex items-center gap-2 w-fit pl-14 pr-5 py-2
+               text-xs text-primary-content hover:text-white
+               bg-primary rounded-md border border-neutral-700
+               shadow-md shadow-slate-950/30 hover:shadow-slate-950/50
+               fill-primary-content hover:fill-white
+               transition-all duration-300"><span class="z-10 pl-1 whitespace-nowrap">Give Dana</span> <!></a>`);function Z(p,t){x(t,!1);let r=L(!1);P(()=>{const a=new Image;a.src=u,a.onload=()=>{C(r,!0)}}),k();var o=D(),c=I(o);{var i=a=>{var e=S();s(a,e)},n=a=>{var e=U();N(e,`background-image: url(${u}); 
+               background-size: 65px 65px; 
+               background-position: -17px center; 
+               background-repeat: no-repeat;`);var v=B(h(e),2);R(v,{icon:j,class:"z-10",dimension:"0.9rem"}),g(e),s(a,e)};M(c,a=>{$(r)?a(n,!1):a(i)})}s(p,o),_()}var V=m(`<div class="w-[200px] h-[3.75rem]
+               bg-primary/30 rounded-md animate-pulse
+               mx-auto inline-flex"></div>`),q=m(`<a href="https://ko-fi.com/meditationonline" target="_blank" class="relative flex items-center gap-2 w-fit pl-14 pr-5 py-2
+               text-xs text-primary-content hover:text-white
+               bg-primary rounded-md border border-neutral-700
+               shadow-md shadow-slate-950/30 hover:shadow-slate-950/50
+               fill-primary-content hover:fill-white
+               transition-all duration-300"><span class="z-10 pl-1 whitespace-nowrap">Card / CashApp</span> <!></a>`);function aa(p,t){x(t,!1);let r=L(!1);P(()=>{const a=new Image;a.src=u,a.onload=()=>{C(r,!0)}}),k();var o=D(),c=I(o);{var i=a=>{var e=V();s(a,e)},n=a=>{var e=q();N(e,`background-image: url(${u}); 
+               background-size: 65px 65px; 
+               background-position: -17px center; 
+               background-repeat: no-repeat;`);var v=B(h(e),2);R(v,{icon:j,class:"z-10",dimension:"0.9rem"}),g(e),s(a,e)};M(c,a=>{$(r)?a(n,!1):a(i)})}s(p,o),_()}export{Z as D,R as I,aa as a};
