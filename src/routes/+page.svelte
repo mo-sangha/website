2782334a5f353d9c21c73f21ffa48e02974c0140
Dorje <dorje@hidden.github.com>
2025-07-08 13:42:14 -0600

    feat: Add a third session at 2pm ET

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,7 +22,7 @@
 			<div class="text-left mx-auto w-fit">
 				<ul class="list-disc">
 					<li>Always free. All are welcome.</li>
-					<li>Twice a day: 9am and 9pm ET.</li>
+					<li>Three times a day: 9am, 2pm, and 9pm ET.</li>
 					<li>Established 2013.</li>
 					<li>
 						Under the guidance of <a
