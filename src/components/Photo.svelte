18bc440e666f9754c06be294ae6ba64dad394f52
Dorje <dorje@hidden.github.com>
2025-12-27 18:23:58 -0700

    feat: Add a "Publications" page with our first book!

--- src/components/Photo.svelte
+++ src/components/Photo.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
 	export let src: string;
 	export let caption: string | undefined = undefined;
+	export let url: string | undefined = undefined;
 </script>
 
 <figure class="w-fit">
-	<img
-		{src}
-		alt={caption || src}
-		class="max-w-[250px] border-primary shadow-sm shadow-slate-500 border-2"
-	/>
+	{#if url}
+		<a href={url} rel="noopener nofollow noreferrer" target="_blank">
+			<img
+				{src}
+				alt={caption || src}
+				class="max-w-[250px] border-primary shadow-sm shadow-slate-500 border-2"
+			/>
+		</a>
+	{:else}
+		<img
+			{src}
+			alt={caption || src}
+			class="max-w-[250px] border-primary shadow-sm shadow-slate-500 border-2"
+		/>
+	{/if}
 	{#if caption}
 		<figcaption class="text-center text-sm text-gray-500">{caption}</figcaption>
 	{/if}
---
+++ src/routes/about/publications/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Photo from '../../../components/Photo.svelte';
+</script>
+
+<div class="h-full w-full flex flex-col gap-8 md:gap-16">
+	<h2 class="page-header">Publications</h2>
+
+	<div class="page-content-section gap-6">
+		<h3 class="font-display text-2xl">Beyond Recognition: Four Phases of Freedom</h3>
+		<div class="bookgrid">
+			<Photo src="/book-four-phases.svg" url="https://mo-sangha.github.io/book/book.pdf" />
+			<div>
+				<p>The complete path to liberation practiced by the Meditation Online sangha.</p>
+				<p>
+					The text details recognition of the nature of mind, post-recognition, and release into
+					complete freedom.
+				</p>
+				<p>
+					<a
+						href="https://mo-sangha.github.io/book/book.pdf"
+						rel="noopener nofollow noreferrer"
+						target="_blank"
+						class="link-hover text-secondary"
+					>
+						Freely available as a PDF.
+					</a>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.bookgrid {
+		display: flex;
+	}
+	.bookgrid div {
+		padding-left: 34px;
+	}
+	.bookgrid p {
+		padding-bottom: 10px;
+	}
+</style>
