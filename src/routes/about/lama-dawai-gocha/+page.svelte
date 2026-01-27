a91ce5820ed3fe8e8fa3b76029d9314e4f60239f
Dorje <dorje@hidden.github.com>
2026-01-27 09:45:00 -0700

    feat: Remove picture of Dawai at his request

--- src/routes/about/lama-dawai-gocha/+page.svelte
+++ src/routes/about/lama-dawai-gocha/+page.svelte
@@ -5,7 +5,6 @@
 <div class="h-full w-full flex flex-col gap-8 md:gap-16">
 	<h2 class="page-header">About Lama Dawai Gocha</h2>
 	<div class="page-content-section">
-		<Photo src="/photo-dawai-gocha.jpg" caption="Lama Dawai Gocha" />
 		<p>
 			Lama Dawai Gocha is a Nyingma Dzogchen Yogi of Tibetan Buddhism. He first took refuge with His
 			Holiness Penor Rinpoche when he was a boy and has continued to practice Buddhism for over 20
