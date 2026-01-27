2991c0df7221e5d4461dbf049b36fc926b333ed7
Dorje <dorje@hidden.github.com>
2026-01-27 09:45:52 -0700

    deploy: feat: Remove picture of Dawai at his request
    a91ce5820ed3fe8e8fa3b76029d9314e4f60239f

--- app/immutable/chunks/BqLUESta.js
+++
@@ -1,1 +0,0 @@
-import{a0 as a}from"./BuXxl6f8.js";a();
---
+++ app/immutable/nodes/4.g8-Fyga4.js
@@ -0,0 +1,5 @@
+import{f as o,a as i}from"../chunks/CPaFQLHO.js";import"../chunks/DCe6AZLu.js";var n=o(`<div class="h-full w-full flex flex-col gap-8 md:gap-16"><h2 class="page-header">About Lama Dawai Gocha</h2> <div class="page-content-section"><p>Lama Dawai Gocha is a Nyingma Dzogchen Yogi of Tibetan Buddhism. He first took refuge with His
+			Holiness Penor Rinpoche when he was a boy and has continued to practice Buddhism for over 20
+			years.</p> <p>He has received teachings from H.H. Dalai Lama, H.H. Penor Rinpoche, Mingyur Rinpoche,
+			Dodrupchen and many others; receiving empowerments and instructions on the full range of
+			Buddhist practices, from Śrāvakayāna to Atiyoga.</p></div></div>`);function t(a){var e=n();i(a,e)}export{t as component};
