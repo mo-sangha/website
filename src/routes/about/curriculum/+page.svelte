47384b63c00df295a2e132bdc4a1df552a8fc34a
Dorje <dorje@hidden.github.com>
2026-05-12 09:16:08 -0600

    feat: Change book recommendation to modern version
    A sangha member reported that "Pointing Out the Nature of Mind" is
    currently out of print and expensive used. This updates the same text to
    a revised version that we have already read together in the sangha.

--- src/routes/about/curriculum/+page.svelte
+++ src/routes/about/curriculum/+page.svelte
@@ -29,7 +29,7 @@
 		<div>
 			<h4 class="font-display text-xl underline">Recommended Readings:</h4>
 			<ul class="list-disc list-inside">
-				<li>"Pointing Out the Nature of Mind" by Khenpo Dongyal Rinpoche</li>
+				<li>"The Nature of Mind: The Dzogchen Instructions of Aro Yeshe Jungne" by Patrul Rinpoche</li>
 				<li>"The Precious Treasury of the Way of Abiding" by Longchenpa</li>
 				<li>"Kunjed Gyalpo"</li>
 			</ul>
@@ -66,7 +66,7 @@
 		<div>
 			<h4 class="font-display text-xl underline">Recommended Readings:</h4>
 			<ul class="list-disc list-inside">
-				<li>"Pointing Out the Nature of Mind" by Khenpo Dongyal Rinpoche</li>
+				<li>"The Nature of Mind: The Dzogchen Instructions of Aro Yeshe Jungne" by Patrul Rinpoche</li>
 				<li>"The Precious Treasury of the Way of Abiding" by Longchenpa</li>
 				<li>"Buddhahood Without Meditation" by Dudjom Lingpa</li>
 				<li>"Kunjed Gyalpo"</li>
@@ -130,75 +130,3 @@
 		</p>
 	</div>
 </div>
-
-<!-- 
-MO Approach Overview: Dzogchen Path to Liberation
-
-1st key point: Recognition of the Nature of Mind  
-Introduction/key point, discerning implications, embracing a larger scope, coming to decisive experience.
-
-Important Points: Introduce practitioners to the nature of mind (rang zhin/svabhavana) as awareness, the inherent Buddha nature present in all experiences.
-
-Practices & Approaches:  
-- Pointing Out Instructions: Utilize direct methods to help practitioners recognize their innate awareness, involving 1-1 dialog and guided meditations on effortless abiding.  
-- Short Sits: Introduce brief meditation sessions focusing on effortless abiding, where practitioners rest in awareness without striving for specific experiences.  Getting adjusted to 'non-doing' and releasing into presence.
-- Inquiry Discussions: Facilitate discussions to explore insights or doubts about the nature of mind.
-
-Recommended Readings:  
-- "Pointing Out the Nature of Mind" by Khenpo Dongyal Rinpoche  
-- "The Precious Treasury of the Way of Abiding" by Longchenpa  
-- “Buddhahood Without Meditation” by Dudjom Lingpa
-- "Kunjed Gyalpo" (The All Good)
-
-Introduction to the Four Samayas
-
-2nd key point: Familiarity and Sustaining the Recognition  
-Four divisions: revealing the key point, discerning implications, embracing a larger scope, coming to decisive experience.
-
-Important Points: Deepen familiarity with the nature of mind and integrate various experiences into pure presence.
-
-Practices & Approaches:  
-- 3 Modes of Liberation: Old friend, snake, thief.  
-- 4 Chokshyaks.  
-- Tilopa Six Words of Advice.  
-- Garab Dorje's 3 Statements.  
-- Effortless Abiding: Continued practice of sitting meditation (stillness/movement/awareness).  
-- Integration Exercises: Engage in mindfulness practices throughout daily activities.  
-- Expanded Inquiry Discussions: Foster discussions on sustaining recognition in challenging moments. Use techniques like 'aware of awareness' and 'looking back at the one who's thinking.'
-
-Recommended Readings:  
-- "Pointing Out the Nature of Mind" by Khenpo Dongyal Rinpoche  
-- "The Precious Treasury of the Way of Abiding" by Longchenpa  
-- “Buddhahood Without Meditation” by Dudjom Lingpa
-- "Kunjed Gyalpo"
-
-3rd key point: Complete Effortlessness  
-Four divisions: revealing the key point, discerning implications, embracing a larger scope, coming to decisive experience.
-
-Important Points: A state of complete ease, allowing awareness to remain prominent.
-
-Practices & Approaches:  
-- Garab Dorje's 3 Statements.  
-- 3rd Mode of Liberation: Thief.  
-- 4 Choshyaks.  
-- Non-meditation: Resting in the natural state.  
-- Resting in Presence: Encourage simple presence to all that arises.  
-- Reflective Inquiry: Facilitate reflections on the experience of letting be.
-
-Recommended Readings:  
-- "The Precious Treasury of the Way of Abiding" by Longchenpa  
-- "The Precious Treasury of the Basic Space of Phenomena" by Longchenpa
-
-4th/final key point: Complete Liberation  
-Total release from fixation, allowing for pure presence and the dissolution of the conditioned self.
-
-Practices & Approaches:  
-- Deep Release Meditation: Relaxed non-attachment and pure presence.  
-- Living in Liberation: Liberation in daily life.  
-- Integration of All Experiences: Discussions around integrating joyful and challenging experiences. Normalizing liberation.
-
-Final Reflection:  
-At the conclusion, we hold a space for practitioners to reflect on their journey. Encourage sharing experiences related to the four samayas and understanding the nature of mind. We foster a community environment for ongoing support in practices beyond the group.
-
-This overview is designed to be accessible and comprehensive, allowing practitioners to explore the profound teachings of the Dzogchen tradition while integrating practices into their everyday lives.
--->
