3e6cf2ea8aafd7af504db5afc899a93957168793
Dorje <dorje@hidden.github.com>
2026-08-11 06:55:52 -0600

    deploy: Revert "feat: Update dana page with new address and text"
    a849b24b8333f45f4a2bb5e33a37dd4b2ad0af3e

---
+++ app/immutable/chunks/B1JOMsSq.js
@@ -0,0 +1,25 @@
+import{f as m,a as s,c as D}from"./C69uCOF2.js";import"./B_Ls6VNY.js";import{o as I}from"./Bg_S89PF.js";import{ao as b,c as h,a as g,t as _,aq as y,ap as C,a3 as L,M as $,i as B,s as M}from"./BNNaYUDa.js";import{i as P}from"./CcNrx45a.js";import{a as J,s as l,b as R}from"./NyMRzZbq.js";import{i as k}from"./DWwkzmMT.js";import{l as z,p as x}from"./B4glzOkC.js";var K=m('<div><svg xmlns="http://www.w3.org/2000/svg"><path></path></svg></div>');function q(c,t){const r=z(t,["children","$$slots","$$events","$$legacy"]),o=z(r,["icon","color","dimension"]);b(t,!1);let p=x(t,"icon",8),i=x(t,"color",8),n=x(t,"dimension",8,"1rem");const[a,e,f,j,G]=p().icon;k();var v=K();J(v,()=>({class:"flex bg-red-50 rounded-full align-middle items-center p-1",...o}));var d=h(v),w=h(d);g(d),g(v),_(()=>{l(d,"height",n()),l(d,"width",n()),l(d,"viewBox",`0 0 ${a} ${e}`),l(w,"fill",i()),l(w,"stroke",i()),l(w,"d",G)}),s(c,v),y()}/*!
+ * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
+ * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+ * Copyright 2024 Fonticons, Inc.
+ */const A={prefix:"fas",iconName:"chevron-right",icon:[320,512,[9002],"f054","M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"]},u=""+new URL("../assets/icon-small.CnzeInJV.png",import.meta.url).href;var N=m(`<div class="w-[200px] h-[3.75rem]
+               bg-primary/30 rounded-md animate-pulse
+               mx-auto inline-flex"></div>`),S=m(`<a href="https://www.paypal.com/paypalme/dawaigocha" target="_blank" class="relative flex items-center gap-2 w-fit pl-14 pr-5 py-2
+               text-xs text-primary-content hover:text-white
+               bg-primary rounded-md border border-neutral-700
+               shadow-md shadow-slate-950/30 hover:shadow-slate-950/50
+               fill-primary-content hover:fill-white
+               transition-all duration-300"><span class="z-10 pl-1 whitespace-nowrap">Give Dana</span> <!></a>`);function Y(c,t){b(t,!1);let r=$(!1);I(()=>{const a=new Image;a.src=u,a.onload=()=>{L(r,!0)}}),k();var o=D(),p=C(o);{var i=a=>{var e=N();s(a,e)},n=a=>{var e=S(),f=M(h(e),2);q(f,{get icon(){return A},class:"z-10",dimension:"0.9rem"}),g(e),_(()=>R(e,`background-image: url(${u}); 
+               background-size: 65px 65px; 
+               background-position: -17px center; 
+               background-repeat: no-repeat;`)),s(a,e)};P(p,a=>{B(r)?a(n,!1):a(i)})}s(c,o),y()}var U=m(`<div class="w-[200px] h-[3.75rem]
+               bg-primary/30 rounded-md animate-pulse
+               mx-auto inline-flex"></div>`),V=m(`<a href="https://ko-fi.com/meditationonline" target="_blank" class="relative flex items-center gap-2 w-fit pl-14 pr-5 py-2
+               text-xs text-primary-content hover:text-white
+               bg-primary rounded-md border border-neutral-700
+               shadow-md shadow-slate-950/30 hover:shadow-slate-950/50
+               fill-primary-content hover:fill-white
+               transition-all duration-300"><span class="z-10 pl-1 whitespace-nowrap">Card / CashApp</span> <!></a>`);function Z(c,t){b(t,!1);let r=$(!1);I(()=>{const a=new Image;a.src=u,a.onload=()=>{L(r,!0)}}),k();var o=D(),p=C(o);{var i=a=>{var e=U();s(a,e)},n=a=>{var e=V(),f=M(h(e),2);q(f,{get icon(){return A},class:"z-10",dimension:"0.9rem"}),g(e),_(()=>R(e,`background-image: url(${u}); 
+               background-size: 65px 65px; 
+               background-position: -17px center; 
+               background-repeat: no-repeat;`)),s(a,e)};P(p,a=>{B(r)?a(n,!1):a(i)})}s(c,o),y()}export{Y as D,q as I,Z as a};
